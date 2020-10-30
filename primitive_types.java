699a4b46fd7842eead0e8b39c8f4e6eae4c83ffa
Ani Asatryan <aniasatryan1996@gmail.com>
2020-10-30 11:39:10 +0400

    Add files via upload

---
+++ primitive_types.java
@@ -0,0 +1,40 @@
+public class primitive_types {
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+
+        //printing some primitive type values below
+        //byte - integer values  8 bit
+        byte val1 = 89;
+        System.out.println(val1);
+
+        //short - integer values 16 bit
+        short val2 = 15000;
+        System.out.println(val2);
+
+        //int - integer values 32 bit
+        int val3 = 2000000000;
+        System.out.println(val3);
+
+        //long - integer values 64 bit
+        // *must put L after value
+        long val4 = 9000000000000000000L;
+        System.out.println(val4);
+
+        //float - floating point values 32-bit
+        // *must put f after value
+        float val5 = 215.5f;
+        System.out.println(val5);
+
+        //double - integer values 64 bit
+        double val6 = 2006.845;
+        System.out.println(val6);
+
+        //boolean - true or false
+        boolean val7 = false;
+        System.out.println(val7);
+
+        //char - unicode character 16-bit, with ''
+        char val8 = 'e';
+        System.out.println(val8);
+    }
+}
